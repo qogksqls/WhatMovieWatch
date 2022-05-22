55c102a3046c6c48a7c3e161cdadcce76ba46492
qoqls1212 <qoqls1212@gmail.com>
2022-05-22 15:17:19 +0900

    주석 수정

--- final-pjt-front/src/store/modules/articles.js
+++ final-pjt-front/src/store/modules/articles.js
@@ -29,13 +29,6 @@ export default {
 
   actions: {
     fetchArticles({ commit, getters }) {
-      /* 게시글 목록 받아오기
-      GET: articles URL (token)
-        성공하면
-          응답으로 받은 게시글들을 state.articles에 저장
-        실패하면
-          에러 메시지 표시
-      */
       axios({
         url: drf.articles.articles(),
         method: 'get',
@@ -46,16 +39,6 @@ export default {
     },
 
     fetchArticle({ commit, getters }, articlePk) {
-      /* 단일 게시글 받아오기
-      GET: article URL (token)
-        성공하면
-          응답으로 받은 게시글들을 state.articles에 저장
-        실패하면
-          단순 에러일 때는
-            에러 메시지 표시
-          404 에러일 때는
-            NotFound404 로 이동
-      */
       axios({
         url: drf.articles.article(articlePk),
         method: 'get',
@@ -71,15 +54,6 @@ export default {
     },
 
     createArticle({ commit, getters }, article) {
-      /* 게시글 생성
-      POST: articles URL (게시글 입력정보, token)
-        성공하면
-          응답으로 받은 게시글을 state.article에 저장
-          ArticleDetailView 로 이동
-        실패하면
-          에러 메시지 표시
-      */
-      
       axios({
         url: drf.articles.articles(),
         method: 'post',
@@ -96,14 +70,6 @@ export default {
     },
 
     updateArticle({ commit, getters }, { pk, title, content}) {
-      /* 게시글 수정
-      PUT: article URL (게시글 입력정보, token)
-        성공하면
-          응답으로 받은 게시글을 state.article에 저장
-          ArticleDetailView 로 이동
-        실패하면
-          에러 메시지 표시
-      */
       axios({
         url: drf.articles.article(pk),
         method: 'put',
@@ -119,17 +85,7 @@ export default {
         })
     },
 
-    deleteArticle({ commit, getters }, articlePk) {
-      /* 게시글 삭제
-      사용자가 확인을 받고
-        DELETE: article URL (token)
-          성공하면
-            state.article 비우기
-            AritcleListView로 이동
-          실패하면
-            에러 메시지 표시
-      */
-      
+    deleteArticle({ commit, getters }, articlePk) {      
       if (confirm('정말 삭제하시겠습니까?')) {
         axios({
           url: drf.articles.article(articlePk),
@@ -145,13 +101,6 @@ export default {
     },
 
     likeArticle({ commit, getters }, articlePk) {
-      /* 좋아요
-      POST: likeArticle URL(token)
-        성공하면
-          state.article 갱신
-        실패하면
-          에러 메시지 표시
-      */
       axios({
         url: drf.articles.likeArticle(articlePk),
         method: 'post',
@@ -162,13 +111,6 @@ export default {
     },
 
 		createComment({ commit, getters }, { articlePk, content, created_at }) {
-      /* 댓글 생성
-      POST: comments URL(댓글 입력 정보, token)
-        성공하면
-          응답으로 state.article의 comments 갱신
-        실패하면
-          에러 메시지 표시
-      */
       const comment = { content, created_at }
 
       axios({
@@ -184,13 +126,6 @@ export default {
     },
 
     updateComment({ commit, getters }, { articlePk, commentPk, content, created_at }) {
-      /* 댓글 수정
-      PUT: comment URL(댓글 입력 정보, token)
-        성공하면
-          응답으로 state.article의 comments 갱신
-        실패하면
-          에러 메시지 표시
-      */
       const comment = { content, created_at }
 
       axios({
@@ -206,14 +141,6 @@ export default {
     },
 
     deleteComment({ commit, getters }, { articlePk, commentPk }) {
-      /* 댓글 삭제
-      사용자가 확인을 받고
-        DELETE: comment URL (token)
-          성공하면
-            응답으로 state.article의 comments 갱신
-          실패하면
-            에러 메시지 표시
-      */
         if (confirm('정말 삭제하시겠습니까?')) {
           axios({
             url: drf.articles.comment(articlePk, commentPk),
