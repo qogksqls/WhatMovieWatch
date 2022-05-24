5e38e8797674e3a82c001719b43c695a1a3165d4
qoqls1212 <qoqls1212@gmail.com>
2022-05-24 09:50:28 +0900

    movieList

--- final-pjt-front/src/views_movie/MovieListView.vue
+++ final-pjt-front/src/views_movie/MovieListView.vue
@@ -8,6 +8,8 @@
 import { mapActions, mapGetters } from 'vuex'
 // import axios from 'axios'
 
+// d4cabca9a0ee31db4bf7d9ef5ffb6064
+// https://api.themoviedb.org/3/movie/550?api_key=d4cabca9a0ee31db4bf7d9ef5ffb6064
 const API_KEY = '30e5384d094325bb014c21f1e7ddd39c'
 const API_URL = 'https://api.themoviedb.org/3/'
 
