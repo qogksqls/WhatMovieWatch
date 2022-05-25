8f131bed639f021e7c80f00073d5073a6a50051a
qoqls1212 <qoqls1212@gmail.com>
2022-05-25 20:20:04 +0900

    게시글 수정 시간 구현

--- final-pjt-front/src/components/community/CommentListItem.vue
+++ final-pjt-front/src/components/community/CommentListItem.vue
@@ -6,8 +6,8 @@
     <span v-if="!isEditing">
       {{ payload.content }}
       <div>
-        <span v-if="comment.created_at === comment.updated_at">{{ comment.created_at }}</span>
-        <span v-if="comment.created_at !== comment.updated_at">{{ comment.updated_at }} (수정됨)</span>
+        <span v-if="comment.created_at === comment.updated_at">{{ comment.created_at.slice(0, 19) }}</span>
+        <span v-if="comment.created_at !== comment.updated_at">{{ comment.updated_at.slice(0, 19) }} (수정됨)</span>
       </div>
     </span>
     <hr>
@@ -37,8 +37,6 @@ export default {
         articlePk: this.comment.article,
         commentPk: this.comment.pk,
         content: this.comment.content,
-        // updated_at: this.comment.created_at.slice(0, 19),
-        updated_at: this.comment.created_at,
       },
     }
   },
